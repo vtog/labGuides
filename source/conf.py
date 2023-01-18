370a8356d34824123ec6377046a288f1d3a0ec7f
vtog <vince@tognaci.net>
2023-01-18 15:42:31 -0600

    fixed css and rtd conf.py settings

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,10 @@ release = 'main'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+import sphinx_rtd_theme
+
 extensions = [
+    'sphinx_rtd_theme',
     'myst_parser',
     'sphinx_copybutton'
 ]
@@ -37,6 +40,7 @@ html_theme = 'sphinx_rtd_theme'
 #import sphinx_pdj_theme
 #html_theme = 'sphinx_pdj_theme'
 #html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+html_theme_path = ["_themes", ]
 html_logo = '_static/redhat.png'
 
 html_static_path = ['_static']
